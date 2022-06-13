610ca6172ca689dac2c978ca117de36baadcc8c6
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2022-06-13 12:00:28 +0300

    @: add constants

---
+++ src/constants/signs.js
@@ -0,0 +1,74 @@
+export const SIGNS = {
+  aries: 'aries',
+  taurus: 'taurus',
+  gemini: 'gemini',
+  cancer: 'cancer',
+  leo: 'leo',
+  virgo: 'virgo',
+  libra: 'libra',
+  scorpio: 'scorpio',
+  sagittarius: 'sagittarius',
+  capricorn: 'capricorn',
+  aquarius: 'aquarius',
+  pisces: 'pisces',
+};
+
+export const SIGNS_TITLES = {
+  [SIGNS.aries]: 'Овен',
+  [SIGNS.taurus]: 'Телец',
+  [SIGNS.gemini]: 'Близнецы',
+  [SIGNS.cancer]: 'Рак',
+  [SIGNS.leo]: 'Лев',
+  [SIGNS.virgo]: 'Дева',
+  [SIGNS.libra]: 'Весы',
+  [SIGNS.scorpio]: 'Скорпион',
+  [SIGNS.sagittarius]: 'Стрелец',
+  [SIGNS.capricorn]: 'Козерог',
+  [SIGNS.aquarius]: 'Водолей',
+  [SIGNS.pisces]: 'Рыбы',
+};
+
+export const SIGNS_BY_TITLES = {
+  [SIGNS_TITLES.aries]: SIGNS.aries,
+  [SIGNS_TITLES.taurus]: SIGNS.taurus,
+  [SIGNS_TITLES.gemini]: SIGNS.gemini,
+  [SIGNS_TITLES.cancer]: SIGNS.cancer,
+  [SIGNS_TITLES.leo]: SIGNS.leo,
+  [SIGNS_TITLES.virgo]: SIGNS.virgo,
+  [SIGNS_TITLES.libra]: SIGNS.libra,
+  [SIGNS_TITLES.scorpio]: SIGNS.scorpio,
+  [SIGNS_TITLES.sagittarius]: SIGNS.sagittarius,
+  [SIGNS_TITLES.capricorn]: SIGNS.capricorn,
+  [SIGNS_TITLES.aquarius]: SIGNS.aquarius,
+  [SIGNS_TITLES.pisces]: SIGNS.pisces,
+};
+
+export const SIGNS_EMOJI = {
+  [SIGNS.aries]: '♈',
+  [SIGNS.taurus]: '♉',
+  [SIGNS.gemini]: '♊',
+  [SIGNS.cancer]: '♋',
+  [SIGNS.leo]: '♌',
+  [SIGNS.virgo]: '♍',
+  [SIGNS.libra]: '♎',
+  [SIGNS.scorpio]: '♏',
+  [SIGNS.sagittarius]: '♐',
+  [SIGNS.capricorn]: '♑',
+  [SIGNS.aquarius]: '♒',
+  [SIGNS.pisces]: '♓',
+};
+
+// const signsTitlesPossessive = {
+//   [signs.aries]: 'Овнов',
+//   [signs.taurus]: 'Тельцов',
+//   [signs.gemini]: 'Близнецов',
+//   [signs.cancer]: 'Раков',
+//   [signs.leo]: 'Львов',
+//   [signs.virgo]: 'Дев',
+//   [signs.libra]: 'Весов',
+//   [signs.scorpio]: 'Скорпионов',
+//   [signs.sagittarius]: 'Стрельцов',
+//   [signs.capricorn]: 'Козерогов',
+//   [signs.aquarius]: 'Водолеев',
+//   [signs.pisces]: 'Рыб',
+// };
--- src/index.js
+++ src/index.js
@@ -1,89 +1,19 @@
+import {
+  SIGNS_TITLES,
+  SIGNS_BY_TITLES,
+  SIGNS_EMOJI,
+} from '@/constants/signs';
 require('dotenv').config();
 const https = require('https');
 const xmlParser = require('xml2json');
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const signs = {
-  aries: 'aries',
-  taurus: 'taurus',
-  gemini: 'gemini',
-  cancer: 'cancer',
-  leo: 'leo',
-  virgo: 'virgo',
-  libra: 'libra',
-  scorpio: 'scorpio',
-  sagittarius: 'sagittarius',
-  capricorn: 'capricorn',
-  aquarius: 'aquarius',
-  pisces: 'pisces',
-};
-
-const signsTitles = {
-  [signs.aries]: 'Овен',
-  [signs.taurus]: 'Телец',
-  [signs.gemini]: 'Близнецы',
-  [signs.cancer]: 'Рак',
-  [signs.leo]: 'Лев',
-  [signs.virgo]: 'Дева',
-  [signs.libra]: 'Весы',
-  [signs.scorpio]: 'Скорпион',
-  [signs.sagittarius]: 'Стрелец',
-  [signs.capricorn]: 'Козерог',
-  [signs.aquarius]: 'Водолей',
-  [signs.pisces]: 'Рыбы',
-};
-
-const signsByTitles = {
-  [signsTitles.aries]: signs.aries,
-  [signsTitles.taurus]: signs.taurus,
-  [signsTitles.gemini]: signs.gemini,
-  [signsTitles.cancer]: signs.cancer,
-  [signsTitles.leo]: signs.leo,
-  [signsTitles.virgo]: signs.virgo,
-  [signsTitles.libra]: signs.libra,
-  [signsTitles.scorpio]: signs.scorpio,
-  [signsTitles.sagittarius]: signs.sagittarius,
-  [signsTitles.capricorn]: signs.capricorn,
-  [signsTitles.aquarius]: signs.aquarius,
-  [signsTitles.pisces]: signs.pisces,
-};
-
-// const signsTitlesPossessive = {
-//   [signs.aries]: 'Овнов',
-//   [signs.taurus]: 'Тельцов',
-//   [signs.gemini]: 'Близнецов',
-//   [signs.cancer]: 'Раков',
-//   [signs.leo]: 'Львов',
-//   [signs.virgo]: 'Дев',
-//   [signs.libra]: 'Весов',
-//   [signs.scorpio]: 'Скорпионов',
-//   [signs.sagittarius]: 'Стрельцов',
-//   [signs.capricorn]: 'Козерогов',
-//   [signs.aquarius]: 'Водолеев',
-//   [signs.pisces]: 'Рыб',
-// };
-
-const signsEmoji = {
-  [signs.aries]: '♈',
-  [signs.taurus]: '♉',
-  [signs.gemini]: '♊',
-  [signs.cancer]: '♋',
-  [signs.leo]: '♌',
-  [signs.virgo]: '♍',
-  [signs.libra]: '♎',
-  [signs.scorpio]: '♏',
-  [signs.sagittarius]: '♐',
-  [signs.capricorn]: '♑',
-  [signs.aquarius]: '♒',
-  [signs.pisces]: '♓',
-};
-
 const welcomeText = `
 Выбери свой знак задиака и получи гороскоп на сегодня!
 
-${Object.keys(signsTitles)
-  .map((item) => `${signsEmoji[item]} ${signsTitles[item]}`)
+${Object.keys(SIGNS_TITLES)
+  .map((item) => `${SIGNS_EMOJI[item]} ${SIGNS_TITLES[item]}`)
   .join('\n\n')
 }
 `;
@@ -121,6 +51,6 @@ bot.help((ctx) => ctx.reply(welcomeText));
 bot.on('text', async (ctx) => {
   const { text } = ctx.update.message;
   const data = await loadHoroscope();
-  return ctx.reply(data.horo[signsByTitles[text]].today);
+  return ctx.reply(data.horo[SIGNS_BY_TITLES[text]].today);
 });
 bot.launch();
